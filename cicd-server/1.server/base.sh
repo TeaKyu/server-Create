7b3929bae79ed45168409a827bae46ebc08b55ac
TaeKyu <qwep0224@gmail.com>
2026-03-16 17:58:41 +0900

    Update base.sh

--- cicd-server/1.server/base.sh
+++ cicd-server/1.server/base.sh
@@ -33,10 +33,18 @@ systemctl enable --now docker
 
 ## ============= 기본설정 END ==============
 
-
+# 도커 권한 주기
+chmod 666 /var/run/docker.sock
+usermod -aG docker jenkins
 
 # 도커 이미지 빌드
 docker build -t my-jenkins .
 
 # 도커 이미지 띄우기
-docker run -d -p 8080:8080 -p 50000:50000 -v /var/run/docker.sock:/var/run/docker.sock --name jenkins 0fb 
+#docker run -d -p 8080:8080 -p 50000:50000 -v /var/run/docker.sock:/var/run/docker.sock --name jenkins 0fb 
+docker run -d --name jenkins \
+  -p 8080:8080 \
+  -p 50000:50000 \
+  -v jenkins_home:/var/jenkins_home \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  my-jenkins:1.0
